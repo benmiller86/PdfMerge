2b6868cc3d163921ba8d082f9b9475c3881845c2
Ben Miller <benmiller86@gmail.com>
2017-10-13 13:30:04 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace PDFMerge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi there! Lets sort this PDF out!");
+            Console.Write("Enter path to odd pages: ");
+            string odd = GetPdfPath();
+
+            Console.Write("Enter path to even pages: ");
+            string even = GetPdfPath();
+
+            Console.WriteLine("Thanks! Merging now...");
+
+            string output = Path.Combine(Path.GetDirectoryName(odd), "merged.pdf");
+
+            string cmd = $"/C pdftk A=\"{odd}\" B=\"{even}\" shuffle A Bend-1 output \"{output}\"";
+            Process.Start("CMD.exe", cmd);
+            Console.WriteLine("Thank you bye!");
+        }
+
+        private static string TrimQuotes(string input) {
+            if(Regex.IsMatch(input, "^\"(.*)\"$"))
+                input = input.Substring(1, input.Length - 2);
+
+            return input;
+        }
+
+        private static string GetPdfPath() {
+            string path = TrimQuotes(Console.ReadLine());
+
+            while(!File.Exists(path) || Path.GetExtension(path) != ".pdf") {
+                Console.Write("Whoops, that PDF couldn't be found, try again: ");
+                path = TrimQuotes(Console.ReadLine());
+            }
+
+            return path;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # PdfMerge
+
+A little tool to merge the two PDF files created when scanning a two sided document using the automatic document feeder.
+
+Just give it the full path to both files and you are good to go!
+
+It assumes that the even pages will be in reverse order, which is what happens when you just flip the stack of pages and shove it in the ADF.
